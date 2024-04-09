7b5d2ba5e28a6c21e9d76c7babf75e7af9ec7c2e
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-04-09 20:31:17 +0330

    Update AnswerCard.tsx

--- components/cards/AnswerCard.tsx
+++ components/cards/AnswerCard.tsx
@@ -1,9 +1,8 @@
-import Link from 'next/link';
-
-import Metric from '../shared/Metric';
-import { formatAndDivideNumber, getTimestamp } from '@/lib/utils';
-import { SignedIn } from '@clerk/nextjs';
-import EditDeleteAction from '../shared/EditDeleteAction';
+import Link from "next/link";
+import Metric from "../shared/Metric";
+import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
+// import { SignedIn } from "@clerk/nextjs";
+// import EditDeleteAction from '../shared/EditDeleteAction';
 
 interface Props {
   clerkId?: string | null;
@@ -28,48 +27,48 @@ const AnswerCard = ({
   question,
   author,
   upvotes,
-  createdAt
+  createdAt,
 }: Props) => {
-  const showActionButtons = clerkId && clerkId === author.clerkId;
+  // const showActionButtons = clerkId && clerkId === author.clerkId;
 
   return (
-    <div className="card-wrapper rounded-[10px] px-11 py-9">
-      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
+    <div className='card-wrapper rounded-[10px] px-11 py-9'>
+      <div className='flex flex-col-reverse items-start justify-between gap-5 sm:flex-row'>
         <Link href={`/question/${question._id}/#${_id}`}>
-          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
+          <span className='subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden'>
             {getTimestamp(createdAt)}
           </span>
-          <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
+          <h3 className='sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1'>
             {question.title}
           </h3>
         </Link>
 
         {/* Delete */}
-        <SignedIn>
+        {/* <SignedIn>
           {showActionButtons && (
-            <EditDeleteAction type="answer" itemId={JSON.stringify(_id)} />
+            <EditDeleteAction type='answer' itemId={JSON.stringify(_id)} />
           )}
-        </SignedIn>
+        </SignedIn> */}
       </div>
 
-      <div className="flex-between mt-6 w-full flex-wrap gap-3">
+      <div className='flex-between mt-6 w-full flex-wrap gap-3'>
         <Metric
           imgUrl={author.picture}
-          alt="user avatar"
+          alt='user avatar'
           value={author.name}
           title={` • asked ${getTimestamp(createdAt)}`}
           href={`/profile/${author.clerkId}`}
-          textStyle="body-medium text-dark400_light700"
+          textStyle='body-medium text-dark400_light700'
           isAuthor
         />
 
-        <div className="flex-center gap-3">
+        <div className='flex-center gap-3'>
           <Metric
-            imgUrl="/assets/icons/like.svg"
-            alt="like icon"
+            imgUrl='/assets/icons/like.svg'
+            alt='like icon'
             value={formatAndDivideNumber(upvotes)}
-            title=" Votes"
-            textStyle="small-medium text-dark400_light800"
+            title=' Votes'
+            textStyle='small-medium text-dark400_light800'
           />
         </div>
       </div>
