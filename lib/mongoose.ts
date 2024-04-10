83232972a99897e603c1d29e983f27cc47171583
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-04-10 17:19:56 +0330

    Update mongoose.ts

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -13,7 +13,7 @@ export const connectToDatabase = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URL, { dbName: "devFlow" });
+    await mongoose.connect(process.env.MONGODB_URL, { dbName: "DevFlow" });
     isConnected = true;
     console.log("🍃 MongoDB is connected 🍃");
   } catch (error) {
