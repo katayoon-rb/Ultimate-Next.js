4f878401bf876e7e06d0751ba8f33fa3636e260f
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-04-20 13:47:11 +0330

    finishing touches 1

--- components/jobs/JobsFilter.tsx
+++ components/jobs/JobsFilter.tsx
@@ -1,20 +1,17 @@
-'use client';
-
-import Image from 'next/image';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-
+"use client";
+import Image from "next/image";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Country } from "@/types";
 import {
   Select,
   SelectContent,
   SelectGroup,
   SelectItem,
   SelectTrigger,
-  SelectValue
-} from '@/components/ui/select';
-import LocalSearchbar from '../shared/search/LocalSearchbar';
-
-import { Country } from '@/types';
-import { formUrlQuery } from '@/lib/utils';
+  SelectValue,
+} from "@/components/ui/select";
+import LocalSearchBar from "../shared/search/LocalSearchBar";
+import { formUrlQuery } from "@/lib/utils";
 
 interface JobsFilterProps {
   countriesList: Country[];
@@ -28,56 +25,54 @@ const JobsFilter = ({ countriesList }: JobsFilterProps) => {
   const handleUpdateParams = (value: string) => {
     const newUrl = formUrlQuery({
       params: searchParams.toString(),
-      key: 'location',
-      value
+      key: "location",
+      value,
     });
 
     router.push(newUrl, { scroll: false });
   };
 
   return (
-    <div className="relative mt-11 flex w-full justify-between gap-5 max-sm:flex-col sm:items-center">
-      <LocalSearchbar
+    <div className='relative mt-11 flex w-full justify-between gap-5 max-sm:flex-col sm:items-center'>
+      <LocalSearchBar
         route={pathname}
-        iconPosition="left"
-        imgSrc="/assets/icons/job-search.svg"
-        placeholder="Job Title, Company, or Keywords"
-        otherClasses="flex-1 max-sm:w-full"
+        iconPosition='left'
+        imgSrc='/assets/icons/job-search.svg'
+        placeholder='Job Title, Company, or Keywords'
+        otherClasses='flex-1 max-sm:w-full'
       />
 
       <Select onValueChange={(value) => handleUpdateParams(value)}>
-        <SelectTrigger className="body-regular light-border background-light800_dark300 text-dark500_light700 line-clamp-1 flex min-h-[56px] items-center gap-3 border p-4 sm:max-w-[210px]">
+        <SelectTrigger className='body-regular light-border background-light800_dark300 text-dark500_light700 line-clamp-1 flex min-h-[56px] items-center gap-3 border p-4 sm:max-w-[210px]'>
           <Image
-            src="/assets/icons/carbon-location.svg"
-            alt="location"
+            src='/assets/icons/carbon-location.svg'
+            alt='location'
             width={18}
             height={18}
           />
-          <div className="line-clamp-1 flex-1 text-left">
-            <SelectValue placeholder="Select Location" />
+          <div className='line-clamp-1 flex-1 text-left'>
+            <SelectValue placeholder='Select Location' />
           </div>
         </SelectTrigger>
 
-        <SelectContent className="body-semibold text-dark500_light700 small-regular max-h-[350px] max-w-[210px] border-none bg-light-900 dark:bg-dark-300">
+        <SelectContent className='body-semibold text-dark500_light700 small-regular max-h-[350px] max-w-[210px] border-none bg-light-900 dark:bg-dark-300'>
           <SelectGroup>
             {countriesList ? (
               countriesList
                 .sort((a, b) =>
-                  ('' + a.name.common).localeCompare(b.name.common)
+                  ("" + a.name.common).localeCompare(b.name.common)
                 )
                 .map((country: Country) => (
                   <SelectItem
                     key={country.name.common}
                     value={country.name.common}
-                    className="cursor-pointer  focus:bg-light-800 dark:focus:bg-dark-400"
+                    className='cursor-pointer  focus:bg-light-800 dark:focus:bg-dark-400'
                   >
                     {country.name.common}
                   </SelectItem>
                 ))
             ) : (
-              <SelectItem value="No results found">
-                No results found
-              </SelectItem>
+              <SelectItem value='No results found'>No results found</SelectItem>
             )}
           </SelectGroup>
         </SelectContent>
